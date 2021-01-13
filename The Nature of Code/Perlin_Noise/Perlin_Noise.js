09200766464b5d0a75ad49e50870e9f56881ea16
donev-stan <77002111+donev-stan@users.noreply.github.com>
2021-01-13 20:24:08 +0200

    Nature of Code started

---
+++ The Nature of Code/Perlin_Noise/Perlin_Noise.js
@@ -0,0 +1,44 @@
+//var xoff = 0;
+//var xoffY = 10000;
+
+var increment = 0.1;
+var start = 0;
+
+function setup() {
+  createCanvas(400,400);
+
+}
+
+
+function draw() {
+  background(51);
+  stroke(255);
+  noFill();
+  beginShape();
+  
+  
+  var xoff = start;
+  for(var x = 0; x < width; x++){
+    stroke(255);
+    //var y = random(height);
+    var y = noise(xoff) * height;
+    vertex(x, y);
+     
+    xoff += increment;
+}
+  
+ 
+    start += increment;  
+  endShape();
+  
+  //noLoop();
+  
+  //var x = map(noise(xoff), 0, 1, 0, width);
+  //var y = map(noise(xoffY), 0, 1, 0, height);
+  
+  //xoff+= 0.01;
+  //xoffY += 0.02;
+  
+  //ellipse(x, y, 24, 24);
+
+}
